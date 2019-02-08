965842cc614a311b0ad6c55705eda655f2cdc110
FesonX <fesonx@foxmail.com>
2019-02-08 13:32:58 +0800

    [Add]Article's Clicked Count by Redis

--- article/list_view.py
+++ article/list_view.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.http import HttpResponse
 from .models import ArticlePost
+from django.conf import settings
 import markdown
+import redis
+
+
+r = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
+
+
+@csrf_exempt
+@require_POST
+@login_required(login_url='/account/login/')
+def like_article(request):
+    article_id = request.POST.get('id')
+    action = request.POST.get("action")
+    if article_id and action:
+        try:
+            article = ArticlePost.objects.get(id=article_id)
+            if action == "wow":
+                article.wow.add(request.user)
+                return HttpResponse("1")
+            else:
+                return HttpResponse("2")
+        except:
+            return HttpResponse("no")
 
 
 def article_titles(request, username=None):
@@ -42,4 +69,13 @@ def article_titles(request, username=None):
 
 def article_detail(request, id, slug):
     article = get_object_or_404(ArticlePost, id=id, slug=slug)
-    return render(request, "article/list/article_detail.html", {'article': article})
+    total_views = r.incr("article:{}:views".format(article.id))
+    r.zincrby('article_ranking', 1, article.id)
+
+    article_ranking = r.zrange('article_ranking', 0, -1, desc=True)[:10]
+    article_ranking_ids = [int(id) for id in article_ranking]
+    most_viewed = list(ArticlePost.objects.filter(id__in=article_ranking_ids))
+    most_viewed.sort(key=lambda x: article_ranking_ids.index(x.id))
+    return render(request, "article/list/article_detail.html", {'article': article,
+                                                                'total_views': total_views,
+                                                                'most_viewed': most_viewed})
