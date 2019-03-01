1478038578f815eb15b04d0a2303491ce1a718a7
FesonX <fesonx@foxmail.com>
2019-03-01 15:19:39 +0800

    [fix]Redirect Error/ favicon 404

--- myBlog/urls.py
+++ myBlog/urls.py
@@ -16,19 +16,21 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^favicon.ico$', RedirectView.as_view(url=r'static/favicon.ico')),
     url(r'^blog/', include('blog.urls')),
     # Using app_name with include is deprecated in Django 1.9
     # and does not work in Django 2.0. Set app_name in account/urls.py instead
     # url(r'^account/', include('account.urls', namespace='account', app_name='account')),
     url(r'^account/', include('account.urls')),
     url(r'^article/', include('article.urls')),
-    url(r'home/', TemplateView.as_view(template_name="index.html"), name="index"),
+    url(r'^$', TemplateView.as_view(template_name="index.html"), name="index"),
     url(r'^image/', include('image.urls')),
 ]
 
