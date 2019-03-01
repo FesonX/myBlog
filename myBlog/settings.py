2249de98dae0cb38b278f21035f8669a1f7766f1
FesonX <fesonx@foxmail.com>
2019-03-01 17:13:50 +0800

    [Update]change sqlite into mysql

--- myBlog/settings.py
+++ myBlog/settings.py
@@ -79,9 +79,18 @@ WSGI_APPLICATION = 'myBlog.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myBlog',
+        'USER': "feson",
+        'PASSWORD': "feson",
+        'HOST': "localhost",
+        'PORT': '3306',
+        'CHARSET': 'utf8',
     }
 }
 
